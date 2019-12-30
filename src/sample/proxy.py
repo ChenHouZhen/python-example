b1f1afa0e3d496bd0d3ccae2bb708d18b3bf7847
chenhouzhen <chenhouzhen@hqjy.com>
2019-12-30 14:45:21 +0800

    del

--- src/sample/proxy.py
+++ src/sample/proxy.py
@@ -1,10 +1,5 @@
 import requests
 
-# 测试结果
-# socks5_remote_proxies 、socks5_local_proxies 两个代理可以获取 baidu 数据而无法获取 google 数据
-# http_proxies 代理既可以获取 baidu 也可以获取 google
-# 为什么？
-
 socks5_remote_proxies = {
     "https": "socks5h://root:123456@192.243.116.107:8080",
     "http": "socks5h://root:123456@192.243.116.107:8080"
